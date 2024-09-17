9d30a1c14fe7b80eebd94e47b93f79adc8273062
Tom Golden <oss@tom.bio>
2024-09-18 00:14:09 +0100

    test: attempt to fix windows tests

--- lib/run.test.ts
+++ lib/run.test.ts
@@ -82,7 +82,7 @@ test("run a file directly (file doesn't exist)", async () => {
     })
   ).resolves.toBe(1);
   expect(stdout).toBe("");
-  expect(stderr).toMatch(`Could not find entry:\n  lib/__fixtures__/a`);
+  expect(stderr).toMatch(`Could not find entry:\n  ${fsPath.join("lib", "__fixtures__", "a")}`);
 });
 
 test("run a file directly (path doesn't exist)", async () => {
@@ -101,7 +101,9 @@ test("run a file directly (path doesn't exist)", async () => {
     })
   ).resolves.toBe(1);
   expect(stdout).toBe("");
-  expect(stderr).toMatch("Could not find entry:\n  lib/__fixtures__/a/b/c");
+  expect(stderr).toMatch(
+    `Could not find entry:\n  ${fsPath.join("lib", "__fixtures__", "a", "b", "c")}`
+  );
 });
 
 test("run a file directly (success with stdout)", async () => {
@@ -126,7 +128,7 @@ test("run a file directly (success with stdout)", async () => {
 test("run a file directly (runtime error)", async () => {
   let stdout = "";
   let stderr = "";
-  const fileRelativePath = "lib/__fixtures__/error-handling/runtime-error.ts";
+  const fileRelativePath = fsPath.join("lib", "__fixtures__", "error-handling", "runtime-error.ts");
   await expect(
     run({
       filePath: fileRelativePath,
@@ -148,7 +150,10 @@ test("run allows stdin passthrough", async () => {
   let hasWritten = false;
   let stdout = "";
   await new Promise<void>((resolve) => {
-    const child = spawn("xnr", ["lib/__fixtures__/stdin/run-wrapper.ts"], { stdio: "pipe" });
+    const child = spawn("xnr", ["lib/__fixtures__/stdin/run-wrapper.ts"], {
+      shell: process.platform === "win32",
+      stdio: "pipe",
+    });
     function write(data: string) {
       if (child.stdin.write(data)) {
         process.nextTick(() => {
